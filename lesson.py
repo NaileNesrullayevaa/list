e3eb2d1e9db9cbb64614284233f9df18ff6ba9d4
NaileNesrullayevaa <nailenesrullayeva536@gmail.com>
2025-04-01 16:23:05 +0400

    first

---
+++ lesson.py
@@ -0,0 +1,82 @@
+ferma =['at','qoyun','inek','at','inek','qoyun','at','at','keci']
+# print(ferma.index('keci'))
+# ------------------------------
+# ferma.sort()
+# print(ferma)
+# ----------------------------------
+# ferma.remove('at')
+# ferma.append('toyuq')
+# print(ferma)
+
+# -------------------------------------
+
+# ferma.insert(0,'keci')
+# print(ferma)
+# ----------------------------------------
+# say=len(ferma)
+# print(ferma[0:say//2])
+
+
+
+
+# 2ci sual---------------------------------------------------------------------
+
+heyvan=['at','qoyun','inek','inek','qoyun']
+# print(heyvan*3)
+# ------------------------------------------
+# heyvan.reverse()
+# print(heyvan)
+# -----------------------------------------------
+
+# inek=heyvan.count('inek')
+# umumi=len(heyvan)
+# print((inek/umumi)*100)
+
+# ---------------------------------------------
+
+# print(heyvan.copy())
+# ------------------------------------------
+# heyvan.clear()
+# print(heyvan)
+
+
+# 3cu sual--------------------------------------------------------------------------------------------
+
+# result=[0,0]
+# numbers=[10.2,-32,-5.45,32,7,43,-24.06,-4,-6.33,62]
+# musbetCem=0
+# menfiCem=0
+# result=[]
+# for i in numbers :
+#     if i>0 :
+#         musbetCem+=i
+#     else :
+#         menfiCem+=i
+# result.append(musbetCem)
+# result.append(menfiCem)
+# print(result)
+
+# 4cu sual----------------------------------------------------------------------------------------------
+
+# number=input('ededi daxil edin:')
+# arr=[]
+# if number.isdigit() :
+#     for i in number :
+#         if int(i)%2!=0 :
+#             arr.append(i)
+#     arr.reverse()
+#     print(arr)
+# else :
+#     print('duzgun eded daxil edin')
+
+# 5ci sual--------------------------------------------------------------------------------------------------
+
+# numbers=[10.2,-32,-5.45,32,7,43,-24.06,-4,-6.33,62]
+# max=numbers[0]
+# min=numbers[0]
+# for i in numbers :
+#     if i>max :
+#         max=i
+#     elif i<min :
+#         min=i
+# print(max,min)
